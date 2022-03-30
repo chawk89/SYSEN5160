395c1d16f58fde0931f867ee1cba7d6a89f3f08f
chawk89 <32115931+chawk89@users.noreply.github.com>
2022-03-29 19:40:13 -0700

    Create nfl_data.py

---
+++ nfl_data.py
@@ -0,0 +1,25 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+import SALib
+from geopy.distance import geodesic
+  
+# Test loading the lat-long data 
+stadium_a = (22.5, 88.3639)
+stadium_b = (28.7041, 77.1025)
+  
+# Print the distance calculated in miles
+print(geodesic(stadium_a, stadium_b).mi)
+
+# open csv file and read it 
+scores = pd.read_csv("../input/nfl-scores-and-betting-data/spreadspoke_scores.csv")
+
+
+stadiums = pd.read_csv("../input/nfl-scores-and-betting-data/nfl_stadiums.csv") 
+nfl_combined = scores.join(stadiums.set_index('stadium_name'), on='stadium')
+# Teachers Version 
+
+nfl_combined.plot()
+
+plt.show()
+
+nfl_combined.plot(kind = 'scatter', x = 'weather_wind_mph', y = 'score_home')
