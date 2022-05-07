3e08333ab9e5b66555e40a76419259c28224287a
chawk89 <32115931+chawk89@users.noreply.github.com>
2022-05-07 15:37:28 -0700

    Update nfl_prediction_app.py

--- nfl_prediction_app.py
+++ nfl_prediction_app.py
@@ -277,8 +277,10 @@ with row4_2:
         "**Get wisdom of the crowd through betting trends**"
     )
   clicked = st.button("Get my final recommendation!")
+  best_bets['handle_percentage_home'] = int(best_bets['handle_percentage_home'])   
+  pick = best_bets[best_bets['handle_percentage_home'] == best_bets.handle_percentage_home.max()]
+     
   if clicked:
-   pick = best_bets[best_bets['handle_percentage_home'] == best_bets.handle_percentage_home.max()]
    st.table(best_bets[['team_home','bet_percentage_home','handle_percentage_home']])
    st.write('**The crowd favorite is: {pick}!**')
 
